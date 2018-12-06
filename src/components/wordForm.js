4babc3e4736761ea83af9ff7ff68b0270f9aede0
Marianne Rachid <marianne.rachid@gmail.com>
2018-12-06 14:58:19 +0000

    Add Anagrams component

--- src/components/wordForm.js
+++ src/components/wordForm.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import Anagrams from './anagram'
 
 export default class Word extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = { word: '' }
+  this.updateWord = this.updateWord.bind(this)
+}
+
+updateWord = (e) => {
+  e.preventDefault();
+  this.setState({
+    word: e.target.value
+  })
+}
+
   render() {
     return (
       <form>
@@ -8,11 +22,9 @@ export default class Word extends React.Component {
           type="text"
           className="word"
           placeholder='Enter your word'
+          onChange={this.updateWord}
           required />
-        <input
-          type="submit"
-          value="Submit"
-           />
+      <Anagrams />
       </form>
     );
   }
